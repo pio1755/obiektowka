a8efcbf9aa0a9a26eab63cc1d45f9fd2671ec5f6
pio1755 <44492933+pio1755@users.noreply.github.com>
2019-10-23 12:21:41 +0200

    Create Program3.cs

---
+++ Program3.cs
@@ -0,0 +1,56 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student pierwszy = new Student("Arek","Nazwiskowy",1999,1,1,132456);
+
+            Console.WriteLine(pierwszy.ObliczWiek());
+            Student drugi = new Student("Mirek", "Nazwiskowykdedk", 2001, 1, 1, 132416);
+            Osoba pierwsz = new Osoba("Jarek", "Nazwiskowydedkk", 2001);
+            Osoba drug = new Osoba("Zbuchu", "Nazwiskowykdwk", 2001);
+            Console.ReadKey();
+         //   pierwszy = pierwsz; nie dziala!!
+         //   drug = drugi; dziala!!
+
+        }
+    }
+}
+
+class Osoba
+{
+    protected String imie;
+    protected String Nazwisko;
+    protected int rok_ur;
+    public Osoba(String imie, String nazwisko, int rok_ur)
+    {
+        this.imie = imie;
+        this.Nazwisko = nazwisko;
+        this.rok_ur = rok_ur;
+
+    }
+    public int ObliczWiek()
+    {
+        int aktWiek;
+        aktWiek = 2019 - rok_ur;
+        return aktWiek;
+    }
+
+
+}
+class Student:Osoba
+{
+    int rok;
+    int numerGrupy;
+    int numerAlbumu;
+    public Student(String imie,String nazwisko,int rok_ur, int rok, int numerGrupy,int numerAlbumu): base(imie, nazwisko, rok_ur)
+    {
+        this.rok = rok;
+        this.numerGrupy = numerGrupy;
+        this.numerAlbumu = numerAlbumu;
+    }
+
+}
