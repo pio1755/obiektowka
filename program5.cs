d677acdf6bdc0a8da04d1e7c41f09bf1a853077a
pio1755 <44492933+pio1755@users.noreply.github.com>
2019-11-13 14:53:16 +0100

    Create program5.cs
    do zad 7

---
+++ program5.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace lista6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pracownik p = new Programista();
+            p.Pracuj();
+            Console.ReadKey();
+
+        }
+    }
+    public class Pracownik
+    {
+
+        public virtual void Pracuj()
+        {
+           Console.WriteLine("logowanie");
+        }
+    }
+    public class Programista : Pracownik
+    {
+        public override void Pracuj()
+        {
+            base.Pracuj();
+            Console.WriteLine("Obowiazki programisty");
+
+        }
+
+    }
+}
